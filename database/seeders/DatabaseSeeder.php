e7f3ed73435eb2848ad60b272f862998dc6b69e1
mfgustav0 <gustavomonteirofavaro@gmail.com>
2023-07-03 20:46:00 -0300

    Database Seed adjustment

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            CustomerSeeder::class,
-            // UserSeeder::class
+            // CustomerSeeder::class,
+            UserSeeder::class
         ]);
     }
 }
